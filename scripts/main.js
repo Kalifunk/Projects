ea9708ec46652eefa1813ea3d7ba310b193825bf
Nathan Sing <nssin@student.unimelb.edu.au>
2020-07-29 18:20:44 +1000

    Fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,50 @@
+const tweets = [
+    "Test tweet1",
+    "Test tweet2",
+    "Test tweet3",
+    "Test tweet4",
+    "Test tweet5",
+];
+const celebrities = {
+    Donald_Trump: ["Test tweet1", "Test tweet2", "Test tweet3"],
+    Hillary_Clinton: ["Test tweet4", "Test tweet5"],
+};
 
-const tweets = ["Test tweet1", "Test tweet2","Test tweet3","Test tweet4","Test tweet5"];
-
-pickRandomTweet(tweets)
+rt = pickRandomTweet(tweets);
 
 function pickRandomTweet(tweets) {
-    var randomTweet = tweets[Math.floor(Math.random()*tweets.length)]
+    var randomTweet = tweets[Math.floor(Math.random() * tweets.length)];
     document.getElementById("tweet").innerHTML = randomTweet;
+    return randomTweet;
 }
 
+function getTweetOwner(tweet) {
+    for (var i in celebrities) {
+        if (celebrities[i].includes(tweet)) {
+            return i;
+        }
+    }
+}
 // 3. Add event handler
-document.getElementById("btn1").addEventListener("click", function(){
-    pickRandomTweet(tweets)
+document.getElementById("btn1").addEventListener("click", function () {
+    //pickRandomTweet(tweets);
+
+    var tweeter = getTweetOwner(rt);
+    if (document.getElementById("btn1").value == tweeter) {
+        alert("You guessed correctly!");
+    } else {
+        alert("You guessed incorrectly!");
+    }
+    rt = pickRandomTweet(tweets);
 });
 
+document.getElementById("btn2").addEventListener("click", function () {
+    //pickRandomTweet(tweets);
+    var tweeter = getTweetOwner(rt);
+    if (document.getElementById("btn2").value == tweeter) {
+        alert("You guessed correctly!");
+    } else {
+        alert("You guessed incorrectly!");
+    }
+    rt = pickRandomTweet(tweets);
+});
